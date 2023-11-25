7501884e253613057842c878ec8593c46e3e660a
niyaben <niyaben.chaudhari@ontariotechu.net>
2023-11-25 03:39:07 -0500

    database table commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports =
 {
-    "URI": "mongodb+srv://niyachaudhari13:h4T9Aq3JyMZb4Zka@cluster0.xhjzlcf.mongodb.net//eventsDB"
+    "URI": "mongodb+srv://niyachaudhari13:h4T9Aq3JyMZb4Zka@cluster0.xhjzlcf.mongodb.net/eventsDB"
 }
