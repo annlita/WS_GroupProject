0466d308305ccdc6aeaf57a0d2d9d695c0abfc9b
susank823 <146685850+susank823@users.noreply.github.com>
2023-12-02 13:26:24 -0500

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,45 @@
-//
+let mongoose = require('mongoose');
+let passportLocalMongoose = require('passport-local-mongoose');
+
+let User = mongoose.Schema({
+    username:
+    {
+        type:String,
+        default:"",
+        trim:true,
+        required:'Username is required'
+    },
+    
+   displayName:
+   {
+        type:String,
+        default:"",
+        trim:true,
+        required:'DisplayName is required'
+   },
+   email:
+   {
+        type:String,
+        default:"",
+        trim:true,
+        required:'Email is required'
+   },
+   created:
+   {
+        type:Date,
+        default: Date.now
+   },
+   update:
+   {
+    type:Date,
+    default: Date.now
+   }
+},
+{
+    collection: "user"
+}
+)
+
+let options = ({MissingPasswordError:'Wrong/Missing Password'});
+User.plugin(passportLocalMongoose,options);
+module.exports.User = mongoose.model('User',User);
