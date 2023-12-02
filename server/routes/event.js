2dc9c2a0584b3d7a3fc27f7e1161fbc5cff3ced8
annlita <annlitajoshy@gmail.com>
2023-12-01 21:19:14 -0500

    pt2 routes commit

--- server/routes/event.js
+++ server/routes/event.js
@@ -7,18 +7,28 @@ let mongoose = require('mongoose');
 let Event = require('../models/event');
 
 let EventController = require('../controller/event')
-/* Get route for the Bio Events list */
+/* Get route for the Events list */
+
+// helper function
+function requireAuth(req,res,next){
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login')
+    }
+    next();
+}
+
 // Read Operation
 router.get('/',EventController.DislayEventlist);
 /* Get route for Add Event page --> Create */
-router.get('/add',EventController.AddEvent); 
+router.get('/add', requireAuth, EventController.AddEvent); 
 /* Post route for Add Event page --> Create */
-router.post('/add',EventController.ProcessEvent);
+router.post('/add', requireAuth, EventController.ProcessEvent);
 /* Get route for displaying the Edit Event page --> Update */
-router.get('/edit/:id',EventController.EditEvent);
+router.get('/edit/:id', requireAuth, EventController.EditEvent);
 /* Post route for processing the Edit Event page --> Update */
-router.post('/edit/:id',EventController.ProcessEditEvent);
+router.post('/edit/:id',requireAuth, EventController.ProcessEditEvent);
 /* Get to perform Delete Operation --> Delete Operation */
-router.get('/delete/:id',EventController.DeleteEvent);
+router.get('/delete/:id', requireAuth, EventController.DeleteEvent);
  module.exports = router;
     
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,14 @@
 let express = require('express');
 let router = express.Router();
 let indexController = require('../controller/index');
+
+
+
+
+
+
+
+
 /* GET home page. */
 router.get('/', indexController.displayHomePage );
 
@@ -16,5 +24,18 @@ router.get('/faculties', indexController.displayFacultiesPage );
 
 /* GET feeback page. */
 router.get('/feedback', indexController.displayFeedbackPage);
+
+/* GET login page. */
+router.get('/login', indexController.displayLoginPage);
+/* POST login page. */
+router.post('/login', indexController.processLoginPage);
+
+/* GET register page. */
+router.get('/register', indexController.displayRegisterPage);
+/* POST register page. */
+router.post('/register', indexController.processRegisterPage);
+
+/* GET logout page. */
+router.get('/logout', indexController.preformLogoutPage);
 
 module.exports = router;
