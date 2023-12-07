cd4ed4d5d5596b11a25d39c229594ed8fcf46c96
Heremela Sitotaw <heremela.sitotaw@ontariotechu.net>
2023-12-07 11:54:44 -0500

    new update

---
+++ client/src/app/pages/faculties/faculties.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-faculties',
+  standalone: true,
+  imports: [],
+  templateUrl: './faculties.component.html',
+  styleUrl: './faculties.component.css'
+})
+export class FacultiesComponent {
+
+}
